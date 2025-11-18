cb8e8f1edb67c096e84ebe5f6cc0b8cb52554d9a
mesameskola <skolamesame82@gmail.com>
2025-11-18 15:07:56 +0400

    homePage

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+<h3>Footer</h3>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora, nobis.</p>    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col gap-[10px]">
+      <h3 className="text-[30px] bg-green-500 rounded-[20px]">Header</h3>
+      <p className="p-[10px] bg-blue-500">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora,
+        nobis.
+      </p>
+    </div>
+  );
+};
+
+export default Header;
